3478a6b81fe8e93828780b779967fd3e247e8570
Nyan Htoo Tha <nyanhtootha@gmail.com>
2020-09-26 22:28:47 +0630

    added latlng onchange js on parent template and tested flask_wtf form and post

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-import jinja2
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import folium
+import jinja2
 
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'this is secret string'
+
+
+class TestForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    latlng = StringField('Clicked location:')
+    submit = SubmitField('Submit')
 
 
 latlngPop = jinja2.Template("""
@@ -25,14 +35,20 @@ latlngPop = jinja2.Template("""
                    {% endmacro %}""")
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    form = TestForm()
     start_coords = (16.79631, 96.16469)
     map = folium.Map(location=start_coords, zoom_start=14)
     el = folium.MacroElement().add_to(map)
     el._template = latlngPop
     map.save('templates/map.html')
-    return render_template('index.html')
+    mapid = "map_"+map._id
+    if form.validate_on_submit():
+        name = form.name.data
+        latlng = form.latlng.data
+        return render_template("index.html", form=form, mapid=mapid, name=name, latlng=latlng)
+    return render_template("index.html", form=form, mapid=mapid)
 
 
 
