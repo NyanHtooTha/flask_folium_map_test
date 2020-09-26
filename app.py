19017cadc216bdfc5051a1156c1f188e8489dde3
Nyan Htoo Tha <nyanhtootha@gmail.com>
2020-09-26 22:08:11 +0630

    added latlng popup javascript macro element

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import jinja2
 import folium
 
 
@@ -8,10 +9,28 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
+latlngPop = jinja2.Template("""
+
+                   {% macro script(this, kwargs) %}
+
+                      var {{this.get_name()}} = L.popup();
+                      function latlngPop(e) {
+                      data = e.latlng.lat.toFixed(6) + ", " + e.latlng.lng.toFixed(6);
+                             {{this.get_name()}}.setLatLng(e.latlng)
+                                                .setContent( "<br/> "+data+" <br/><a href="+data+">Click Here</a>")
+                                                .openOn({{this._parent.get_name()}})
+                             }
+                      {{this._parent.get_name()}}.on('click', latlngPop);
+
+                   {% endmacro %}""")
+
+
 @app.route('/')
 def index():
     start_coords = (16.79631, 96.16469)
     map = folium.Map(location=start_coords, zoom_start=14)
+    el = folium.MacroElement().add_to(map)
+    el._template = latlngPop
     map.save('templates/map.html')
     return render_template('index.html')
 
