9a0b1e359a4c480f8274e5529483a31c83663fb3
Nyan Htoo Tha <nyanhtootha@gmail.com>
2020-10-28 14:51:18 +0630

    added draw tools on test

--- app.py
+++ app.py
@@ -192,6 +192,8 @@ def route_map():
 def test():
     start_coords = (16.79631, 96.16469)
     map_tem = folium.Map(location=start_coords, zoom_start=14)
+    draw = plugins.Draw(export=True)
+    draw.add_to(map_tem)
     map_tem.save('templates/map.html')
     return render_template("test.html")
 
