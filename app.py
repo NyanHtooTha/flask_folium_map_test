061eb377e03fe8556e4016d236f4b06b845dc3de
Nyan Htoo Tha <nyanhtootha@gmail.com>
2020-11-01 18:10:14 +0630

    split leafletjs elements scripts

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 from wtforms.widgets import TextArea
 import folium
 from folium import plugins
-import jinja2
+from leafletjs_elements import elements
 from calculate import get_mid_point
 
 
@@ -36,103 +36,6 @@ class ExpressForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-set_latlng_locate = jinja2.Template("""
-
-                   {% macro script(this, kwargs) %}
-
-                      parentWindow = window.parent;
-                      var former_marker;
-
-                      function remove_marker() { if (former_marker){ {{this._parent.get_name()}}.removeLayer(former_marker); } }
-
-                      {{this._parent.get_name()}}.on('click', function(e) {
-                          if (former_marker){ {{this._parent.get_name()}}.removeLayer(former_marker); }
-                          icon = L.AwesomeMarkers.icon(
-                                     {"extraClasses": "fa-rotate-0", "icon": "check",
-                                      "iconColor": "white", "markerColor": "red", "prefix": "glyphicon"});
-                          var new_marker = L.marker().setLatLng(e.latlng).setIcon(icon).addTo({{this._parent.get_name()}});
-                          new_marker.on('dblclick', function(e){
-                              {{this._parent.get_name()}}.removeLayer(e.target)
-                              parentWindow.document.getElementById("latlng").value = "";
-                          });
-                          var lat = e.latlng.lat.toFixed(4), lng = e.latlng.lng.toFixed(4);
-                          new_marker.bindPopup("Latitude: " + lat + "<br>Longitude: " + lng );
-                          former_marker = new_marker;
-
-                          parentWindow.document.getElementById("latlng").value = "";
-                          data = lat + ", " + lng;
-                          setTimeout( function() {
-                          parentWindow.document.getElementById("latlng").value = data;
-                          }, 2000);
-                      });
-
-                      {{this._parent.get_name()}}.on('locationfound', function (e) {
-                          data = e.latlng.lat.toFixed(4) + ", " + e.latlng.lng.toFixed(4);
-                          setTimeout( function() {
-                          parentWindow.document.getElementById("locate").value = data;
-                          }, 2000);
-
-                          var marker  = L.marker([e.latlng.lat, e.latlng.lng],
-                                                 {}
-                                                 ).addTo({{this._parent.get_name()}});
-                          var popup = L.popup().setLatLng(e.latlng)
-                                               .setContent("<b>Your Current Location</b><br>" +
-                                                           "<br>Latitude: " + e.latlng.lat.toFixed(4) +
-                                                           "<br>Longitude: " + e.latlng.lng.toFixed(4))
-                                               .openOn({{this._parent.get_name()}});
-                          marker.bindPopup(popup);
-                      });
-
-                   {% endmacro %}""")
-
-
-set_express_locations = jinja2.Template("""
-
-                   {% macro script(this, kwargs) %}
-
-                      parentWindow = window.parent;
-                      var sender_marker=false, receiver_marker=false;
-
-                      {{this._parent.get_name()}}.on('click', function(e) {
-                          function get_icon(color) {
-                              icon = L.AwesomeMarkers.icon(
-                                         {"extraClasses": "fa-rotate-0", "icon": "check",
-                                          "iconColor": "white", "markerColor": color, "prefix": "glyphicon"});
-                               return icon;
-                          }
-
-                          var lat = e.latlng.lat.toFixed(4), lng = e.latlng.lng.toFixed(4);
-                          var data = lat + ", " + lng;
-                          if (!sender_marker) {
-                              icon = get_icon('green');
-                              sender_marker = L.marker().setLatLng(e.latlng).setIcon(icon).addTo({{this._parent.get_name()}});
-                              sender_marker.bindPopup("Latitude: " + lat + "<br>Longitude: " + lng );
-                              setTimeout( function() {
-                                  parentWindow.document.getElementById("sender_info-location").value = data;
-                              }, 2000);
-                              sender_marker.on('dblclick', function(e){
-                                  {{this._parent.get_name()}}.removeLayer(e.target);
-                                  parentWindow.document.getElementById("sender_info-location").value = "";
-                                  sender_marker = false;
-                              });
-                          } else if (!receiver_marker) {
-                              icon = get_icon('red');
-                              receiver_marker = L.marker().setLatLng(e.latlng).setIcon(icon).addTo({{this._parent.get_name()}});
-                              receiver_marker.bindPopup("Latitude: " + lat + "<br>Longitude: " + lng );
-                              setTimeout( function() {
-                                  parentWindow.document.getElementById("receiver_info-location").value = data;
-                              }, 2000);
-                              receiver_marker.on('dblclick', function(e){
-                                  {{this._parent.get_name()}}.removeLayer(e.target);
-                                  parentWindow.document.getElementById("receiver_info-location").value = "";
-                                  receiver_marker = false;
-                              });
-                          } else {}
-                      });
-
-                   {% endmacro %}""")
-
-
 @app.route('/', methods=["GET", "POST"])
 def index():
     form = TestForm()
@@ -160,7 +63,7 @@ def index():
     #map_tem.add_child(folium.ClickForMarker())
     map_tem.add_child(plugins.LocateControl())
     el = folium.MacroElement().add_to(map_tem)
-    el._template = set_latlng_locate
+    el._template = elements["set_latlng_locate"]
     map_tem.save('templates/map.html')
     if form.validate_on_submit():
         session["name"] = form.name.data
@@ -178,7 +81,7 @@ def express():
     start_coords = (16.79631, 96.16469)
     map_tem = folium.Map(location=start_coords, zoom_start=14)
     el = folium.MacroElement().add_to(map_tem)
-    el._template = set_express_locations
+    el._template = elements["set_express_locations"]
     map_tem.save('templates/map.html')
     return render_template('express.html', form=form)
 
@@ -186,100 +89,6 @@ def express():
 @app.route('/map')
 def route_map():
     return render_template("map.html")
-
-
-drawn_element = jinja2.Template("""
-
-{% macro script(this, kwargs) %}
-
-{{this._parent.get_name()}}.on('draw:created', function (event) {
-    var layer = event.layer,
-    feature = layer.feature = layer.feature || {}; // Intialize layer.feature
-
-    feature.type = feature.type || "Feature"; // Intialize feature.type
-    var props = feature.properties = feature.properties || {}; // Intialize feature.properties
-    props.shape_name = null;
-    props.shape_desc = null;
-    drawnItems.addLayer(layer);
-    add_shape_popup(layer);
-});
-
-function make_content_html(layer) {
-    var l = layer.toGeoJSON();
-    var name = "Shape Name";
-    var desc = "Shape Description";
-    if (l.geometry.type == "LineString") {
-        name = "Path Name";
-        desc = "Path Description";
-    }
-    if (l.geometry.type == "Point") {
-        name = "Point Name";
-        desc = "Point Description";
-    }
-    if (layer._radius || l.geometry.type == "Polygon") {
-        name = "Area Name";
-        desc = "Area Descritption";
-    }
-
-    var content_html = `<span><b>${name}</b></span><br/>`+
-                       '<input id="shape_name" type="text" size="25" /><br/><br/>'+
-                       `<span><b>${desc}<b/></span><br/>`+
-                       '<textarea id="shape_desc" cols="25" rows="5" style="resize:none;" ></textarea><br/><br/>'+
-                       '<div><input class="edit" type="button" value="Edit" style="display:none;" />'+
-                       '<input id="okBtn" class="save" type="button" value="Save" />'+
-                       '<input class="cancel" type="button" value="Cancel" style="display:none;" /></div>'
-    return content_html;
-}
-
-function add_shape_popup(layer) {
-     var content_html = make_content_html(layer);
-     var content = document.createElement("div");
-     content.innerHTML = content_html;
-     layer.bindPopup(content).openPopup();
-
-    $('.edit').click(function() {
-        $('#shape_name').attr('readonly', false);
-        $('#shape_desc').attr('readonly', false);
-        $(this).hide().siblings('.save, .cancel').show();
-    });
-    $('.cancel').click(function() {
-        $('#shape_name').val(layer.feature.properties.shape_name);
-        $('#shape_desc').val(layer.feature.properties.shape_desc);
-        $('#shape_name').attr('readonly', true);
-        $('#shape_desc').attr('readonly', true);
-        $(this).siblings('.edit').show();
-        $(this).siblings('.save').hide();
-        $(this).hide();
-        //layer.closePopup();
-    });
-    $('.save').click(function() {
-        save_shape_name_desc(layer);
-        $(this).siblings('.edit').show();
-        $(this).siblings('.cancel').hide();
-        $(this).hide();
-    });
-
-    //document.getElementById("okBtn").addEventListener("click", function() {
-    //    save_shape_name_desc(layer);
-    //}, false);
-
-    layer.on("popupopen", function () {
-        $('#shape_name').val(layer.feature.properties.shape_name);
-        $('#shape_desc').val(layer.feature.properties.shape_desc);
-        content.focus()
-    });
-}
-
-function save_shape_name_desc(layer) {
-     layer.feature.properties.shape_name = document.getElementById("shape_name").value;
-     layer.feature.properties.shape_desc = document.getElementById("shape_desc").value;
-     document.getElementById("shape_name").readOnly = "true";
-     document.getElementById("shape_desc").readOnly = "true";
-     //layer.closePopup();
-     //console.log(layer.toGeoJSON());
-}
-
-{% endmacro %}""")
 
 
 @app.route('/test')
@@ -365,7 +174,7 @@ def test():
     plugins.MiniMap(tile_layer=basemaps['Google Satellite'], toggle_display=True, width=300, height=300, \
                     zoom_level_offset= -5).add_to(map_tem)
     el = folium.MacroElement().add_to(map_tem)
-    el._template = drawn_element
+    el._template = elements["drawn_element"]
     map_tem.save('templates/map.html')
     return render_template("test.html")
 
---
+++ leafletjs_elements.py
@@ -0,0 +1,209 @@
+import jinja2
+
+
+
+set_latlng_locate = jinja2.Template("""
+
+{% macro script(this, kwargs) %}
+
+parentWindow = window.parent;
+var former_marker;
+
+function remove_marker() { if (former_marker){ {{this._parent.get_name()}}.removeLayer(former_marker); } }
+
+{{this._parent.get_name()}}.on('click', function(e) {
+    
+    if (former_marker){ {{this._parent.get_name()}}.removeLayer(former_marker); }
+    
+    icon = L.AwesomeMarkers.icon(
+               {"extraClasses": "fa-rotate-0", "icon": "check",
+                "iconColor": "white", "markerColor": "red", "prefix": "glyphicon" }
+           );
+           
+           var new_marker = L.marker().setLatLng(e.latlng).setIcon(icon).addTo({{this._parent.get_name()}});
+           new_marker.on('dblclick', function(e) {
+               {{this._parent.get_name()}}.removeLayer(e.target);
+               parentWindow.document.getElementById("latlng").value = "";
+           });
+           
+           var lat = e.latlng.lat.toFixed(4), lng = e.latlng.lng.toFixed(4);
+           new_marker.bindPopup("Latitude: " + lat + "<br>Longitude: " + lng );
+           former_marker = new_marker;
+
+           parentWindow.document.getElementById("latlng").value = "";
+           var data = lat + ", " + lng;
+           setTimeout( function() {
+               parentWindow.document.getElementById("latlng").value = data;
+           }, 2000);
+});
+
+{{this._parent.get_name()}}.on('locationfound', function (e) {
+                          
+    var data = e.latlng.lat.toFixed(4) + ", " + e.latlng.lng.toFixed(4);
+    setTimeout( function() {
+        parentWindow.document.getElementById("locate").value = data;
+    }, 2000);
+
+    var marker  = L.marker([e.latlng.lat, e.latlng.lng], {}).addTo({{this._parent.get_name()}});
+    var popup = L.popup().setLatLng(e.latlng)
+                         .setContent("<b>Your Current Location</b><br>" +
+                                     "<br>Latitude: " + e.latlng.lat.toFixed(4) +
+                                     "<br>Longitude: " + e.latlng.lng.toFixed(4))
+                         .openOn({{this._parent.get_name()}});
+    marker.bindPopup(popup);
+});
+
+{% endmacro %}""")
+
+
+set_express_locations = jinja2.Template("""
+
+{% macro script(this, kwargs) %}
+
+    parentWindow = window.parent;
+    var sender_marker=false, receiver_marker=false;
+
+    {{this._parent.get_name()}}.on('click', function(e) {
+        function get_icon(color) {
+            var icon = L.AwesomeMarkers.icon(
+                           {"extraClasses": "fa-rotate-0", "icon": "check",
+                            "iconColor": "white", "markerColor": color, "prefix": "glyphicon"});
+                            return icon;
+        }
+
+    var lat = e.latlng.lat.toFixed(4), lng = e.latlng.lng.toFixed(4);
+    var data = lat + ", " + lng;
+    if (!sender_marker) {
+        var icon = get_icon('green');
+        var sender_marker = L.marker().setLatLng(e.latlng).setIcon(icon).addTo({{this._parent.get_name()}});
+        sender_marker.bindPopup("Latitude: " + lat + "<br>Longitude: " + lng );
+        
+        setTimeout( function() {
+            parentWindow.document.getElementById("sender_info-location").value = data;
+        }, 2000);
+        
+        sender_marker.on('dblclick', function(e) {
+            {{this._parent.get_name()}}.removeLayer(e.target);
+            parentWindow.document.getElementById("sender_info-location").value = "";
+            sender_marker = false;
+        });
+    } 
+    else if (!receiver_marker) {
+        var icon = get_icon('red');
+        receiver_marker = L.marker().setLatLng(e.latlng).setIcon(icon).addTo({{this._parent.get_name()}});
+        receiver_marker.bindPopup("Latitude: " + lat + "<br>Longitude: " + lng );
+                              
+        setTimeout( function() {
+            parentWindow.document.getElementById("receiver_info-location").value = data;
+        }, 2000);
+                              
+        receiver_marker.on('dblclick', function(e) {
+            {{this._parent.get_name()}}.removeLayer(e.target);
+            parentWindow.document.getElementById("receiver_info-location").value = "";
+            receiver_marker = false;
+        });
+    } 
+    else {}
+});
+
+{% endmacro %}""")
+
+
+drawn_element = jinja2.Template("""
+
+{% macro script(this, kwargs) %}
+
+{{this._parent.get_name()}}.on('draw:created', function (event) {
+    var layer = event.layer,
+    feature = layer.feature = layer.feature || {}; // Intialize layer.feature
+
+    feature.type = feature.type || "Feature"; // Intialize feature.type
+    var props = feature.properties = feature.properties || {}; // Intialize feature.properties
+    props.shape_name = null;
+    props.shape_desc = null;
+    drawnItems.addLayer(layer);
+    add_shape_popup(layer);
+});
+
+function make_content_html(layer) {
+    var l = layer.toGeoJSON();
+    var name = "Shape Name";
+    var desc = "Shape Description";
+    if (l.geometry.type == "LineString") {
+        name = "Path Name";
+        desc = "Path Description";
+    }
+    if (l.geometry.type == "Point") {
+        name = "Point Name";
+        desc = "Point Description";
+    }
+    if (layer._radius || l.geometry.type == "Polygon") {
+        name = "Area Name";
+        desc = "Area Descritption";
+    }
+
+    var content_html = `<span><b>${name}</b></span><br/>`+
+                       '<input id="shape_name" type="text" size="25" /><br/><br/>'+
+                       `<span><b>${desc}<b/></span><br/>`+
+                       '<textarea id="shape_desc" cols="25" rows="5" style="resize:none;" ></textarea><br/><br/>'+
+                       '<div><input class="edit" type="button" value="Edit" style="display:none;" />'+
+                       '<input id="okBtn" class="save" type="button" value="Save" />'+
+                       '<input class="cancel" type="button" value="Cancel" style="display:none;" /></div>'
+    return content_html;
+}
+
+function add_shape_popup(layer) {
+     var content_html = make_content_html(layer);
+     var content = document.createElement("div");
+     content.innerHTML = content_html;
+     layer.bindPopup(content).openPopup();
+
+    $('.edit').click(function() {
+        $('#shape_name').attr('readonly', false);
+        $('#shape_desc').attr('readonly', false);
+        $(this).hide().siblings('.save, .cancel').show();
+    });
+    $('.cancel').click(function() {
+        $('#shape_name').val(layer.feature.properties.shape_name);
+        $('#shape_desc').val(layer.feature.properties.shape_desc);
+        $('#shape_name').attr('readonly', true);
+        $('#shape_desc').attr('readonly', true);
+        $(this).siblings('.edit').show();
+        $(this).siblings('.save').hide();
+        $(this).hide();
+        //layer.closePopup();
+    });
+    $('.save').click(function() {
+        save_shape_name_desc(layer);
+        $(this).siblings('.edit').show();
+        $(this).siblings('.cancel').hide();
+        $(this).hide();
+    });
+
+    //document.getElementById("okBtn").addEventListener("click", function() {
+    //    save_shape_name_desc(layer);
+    //}, false);
+
+    layer.on("popupopen", function () {
+        $('#shape_name').val(layer.feature.properties.shape_name);
+        $('#shape_desc').val(layer.feature.properties.shape_desc);
+        content.focus()
+    });
+}
+
+function save_shape_name_desc(layer) {
+     layer.feature.properties.shape_name = document.getElementById("shape_name").value;
+     layer.feature.properties.shape_desc = document.getElementById("shape_desc").value;
+     document.getElementById("shape_name").readOnly = "true";
+     document.getElementById("shape_desc").readOnly = "true";
+     //layer.closePopup();
+     //console.log(layer.toGeoJSON());
+}
+
+{% endmacro %}""")
+
+
+elements = dict( set_latlng_locate=set_latlng_locate,
+                 set_express_locations=set_express_locations,
+                 drawn_element=drawn_element
+               )
